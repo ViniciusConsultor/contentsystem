135169066cbc34efa9cf8f5bd923b7dd7b5868b7
fabio.gusmao <fabio.gusmao@15c20ce5-7554-0410-b8b4-67ef32aab22e>
2008-09-17 04:44:57 +0000

    DIagrama de Classes Mudanças nas classes

--- trunk/src/SVCE.Modelo/Dados/RelatorioProdutosSemEstoque.cs
+++ trunk/src/SVCE.Modelo/Dados/RelatorioProdutosSemEstoque.cs
@@ -7,19 +7,34 @@ namespace SVCE.Modelo.Dados
 {
     public class RelatorioProdutosSemEstoque
     {
-        public ItemRelatorioProdutosSemEstoque[] Produtos { get; set; }
+        //public ItemRelatorioProdutosSemEstoque[] Produtos { get; set; }
+        public ItemRelatorioProdutosSemEstoque[] Produtos;
+
+
+        public void GerarRelatorio()
+        {
+            return null;
+        }
+
     }
 
     public class ItemRelatorioProdutosSemEstoque
     {
-        public int IdProduto { get; set; }
-        public string NomeProduto { get; set; }
-        public int IdFornecedor { get; set; }
+        //public int IdProduto { get; set; }
+        //public string NomeProduto { get; set; }
+        //public int IdFornecedor { get; set; }
+        //public string NomeFornecedor { get; set; }
+        //public int QuantidadeDisponivel { get; set; }
+        //public int QuantidadeMinima { get; set; }
+        //public decimal PrecoVenda { get; set; }
 
-        public string NomeFornecedor { get; set; }
-        public int QuantidadeDisponivel { get; set; }
-        public int QuantidadeMinima { get; set; }
-        public decimal PrecoVenda { get; set; }
+        public int IdProduto;
+        public string NomeProduto;
+        public int IdFornecedor;
+        public string NomeFornecedor;
+        public int QuantidadeDisponivel;
+        public int QuantidadeMinima;
+        public decimal PrecoVenda;
 
     }
 }
