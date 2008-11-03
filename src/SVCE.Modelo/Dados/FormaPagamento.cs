b95f738bd714e601b78351c8d182265220af24cd
fabio.gusmao <fabio.gusmao@15c20ce5-7554-0410-b8b4-67ef32aab22e>
2008-11-03 00:04:03 +0000

    Inclui formas de pagamento no banco. Criei HTML da tela de venda.

---
+++ src/SVCE.Modelo/Dados/FormaPagamento.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SVCE.Modelo.Dados
+{
+	public class FormaPagamento
+	{
+
+		public int ID { get; set; }
+		public string Descricao { get; set; }
+
+		public static FormaPagamento[] Listar(BancoDeDados banco)
+		{
+			List<FormaPagamento> formas = new List<FormaPagamento>();
+
+			string sql = "SELECT * FROM FORMA_PAGAMENTO";
+
+			var cmd = banco.CriarComando(sql, System.Data.CommandType.Text);
+			var reader = cmd.ExecuteReader();
+
+			while (reader.Read())
+			{
+				FormaPagamento forma = new FormaPagamento();
+				forma.ID = reader.GetInt32(0);
+				forma.Descricao = reader.GetString(1);
+				formas.Add(forma);
+			}
+			reader.Close();
+
+			return formas.ToArray();
+		}
+
+		
+	}
+}
