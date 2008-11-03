a2e6cf5e3ff3674c5dc7086a80243ec91d7d1c41
jeremias.ms <jeremias.ms@15c20ce5-7554-0410-b8b4-67ef32aab22e>
2008-11-03 21:34:19 +0000

    Ae, gusmão funfando!!!

---
+++ trunk/src/SVCE.Controle/CasosDeUso/RealizarPedido.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SVCE.Modelo.Dados;
+namespace SVCE.Controle.CasosDeUso
+{
+    public class RealizarPedido
+    {
+        public Estoque[] ListarBaixoEstoque()
+        {
+            BancoDeDados b = new BancoDeDados();
+            try
+            {
+                b.Conectar();
+                return Estoque.ListarProdutosAbaixoEstoque(b);
+            }
+            finally
+            {
+                b.Desconectar();
+            }
+        }
+    }
+}
